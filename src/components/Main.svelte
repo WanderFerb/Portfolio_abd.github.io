7277bb0a68d619b7ce1e5df6521655d1ebae97ae
Abhinav Dhiman <abhinav-ext@skyeair.tech>
2023-10-31 13:51:59 +0530

    first commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,195 @@
+<script>
+	import Step from '../components/Step.svelte';
+
+	let steps = [
+		{ name: 'SkyeOps', icon: 'fa-brands fa-phoenix-squadron' },
+		{ name: 'SkyeFMS', icon: 'fa-solid fa-fire-flame-curved' },
+		{ name: 'Order Point', icon: 'fa-solid fa-cart-shopping' }
+	];
+
+	let benefits = [
+		{
+			name: 'A self taught developper',
+			description:
+				'I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.'
+		},
+		{
+			name: 'A product & UX fanatic ',
+			description:
+				'Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences.'
+		},
+		{
+			name: 'An excellent communicator',
+			description:
+				"Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team."
+		}
+	];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+	<section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+		<div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg gap-10">
+			<h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+				Hi! I'm <span class="poppins text-violet-400">Abhinav</span> Dhiman
+				<br />
+				Full Stack <span class="poppins text-violet-400">Developer</span>
+			</h2>
+			<p class="text-base sm:text-lg md:text-xl">
+				My <span class="text-violet-400">favourite tech's</span> includes TypeScript(Angular), JavaScript(SvelteKit),
+				Material Libraries, Spring Boot(Java), MySQL, MongoDB & Firebase/Firestore!
+			</p>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<button
+				href="#"
+				target="_blank"
+				class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+			>
+				<div
+					class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+				/>
+				<h4 class="relative z-9">Get in touch &rarr;</h4>
+			</button>
+		</div>
+		<div class="relative shadow-2xl grid place-items-center">
+			<img
+				src={'images/profile.png'}
+				alt="Profile"
+				class="object-cover z-[2] h-[69vh] max-h-[70vh]"
+			/>
+		</div>
+	</section>
+	<section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+		<div class="flex flex-col gap-2 text-center">
+			<h6 class="text-lg sm:text-xl md:text-2xl">A few of my creative endeavours</h6>
+			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+				Curious to <span class="poppins text-violet-400">see</span> my work?
+			</h3>
+		</div>
+		<a
+			href="#"
+			class="mx-auto px-4 py-2 rounded-md border border-solid border-white itmes-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200 flex flex-row"
+		>
+			<i class="fa-regular fa-circle-play" />
+			<p>Watch the portfolio</p>
+		</a>
+		<div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+			<Step step={steps[0]}>
+				<p>
+					SkyeOps is an advanced flight portal that empowers clients to effortlessly request drone
+					flights based on their specific packages incorporating <strong class="text-violet-400"
+						>real-time drone tracking, payload optimization for flying in clusters etc.</strong
+					> SkyeOps has been built to ensure clients receive live updates on flight statuses for enhanced
+					transparency and convenience.
+				</p>
+			</Step>
+			<Step step={steps[1]}>
+				<p>
+					SkyeFMS is a comprehensive Fleet Management System that seamlessly integrates Skytunnels,
+					Tickets, Drones, Pilots, Organizations & Clients which includes but is not limited to<strong
+						class="text-violet-400"
+						>drone tracking & transport processes, Restricted access among various individuals!</strong
+					> SkyeFMS facilitates easy creation and updating of Skytunnels, complete with buffer regions,
+					to establish safe operating spaces for drone operations.
+				</p>
+			</Step>
+			<Step step={steps[2]}>
+				<p>
+					Order Point is a food ordering app providing take-away and dine-in selections created with <strong
+						class="text-violet-400">Ionic Commerce.js Stripe & php!</strong
+					> Commerce.js is a commerce CMS and Stripe is used for all transaction handling.
+				</p>
+			</Step>
+		</div>
+	</section>
+	<section
+		id="about"
+		class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+	>
+		<div
+			class="flex flex-col gap-2 py-8 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700"
+		>
+			<h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+				A bit <span class="poppins text-violet-400">about</span> me.
+			</h3>
+		</div>
+		<p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I am . . .</p>
+		<div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+			{#each benefits as benefit, index}
+				<div class="flex gap-6 sm:gap-8">
+					<p class="poppinstext-4xl sm:text-5xl md:text-6xl text-slate-500 fornt-semibold">
+						0{index + 1}
+					</p>
+					<div class="flex flex-col gap-6 sm:gap-8">
+						<h3 class="text-2xl sm:text-3xl md:text-5xl">
+							{benefit.name}
+						</h3>
+						<p>{benefit.description}</p>
+					</div>
+				</div>
+			{/each}
+		</div>
+		<h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">
+			The <span class="text-violet-400 poppins">Complete</span> Package
+		</h5>
+		<div class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full">
+			<table class="bg-white text-slate-700 rounded text-center">
+				<thead class="border-b border-solid border slate-200">
+					<tr class="">
+						<th />
+						<th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+						<th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+						<th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+						<th class="whitespace-nowrap bg-violet-700 text-white p-2 px-4">Me</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+							>Dedication</td
+						>
+						<td><i class="fa-solid fa-check text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+							>Critical Thought</td
+						>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+							>Interpersonal Skills</td
+						>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap"
+							>Programming Ability</td
+						>
+						<td><i class="fa-solid fa-check text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+			<p>Scroll to see more &rarr;</p>
+		</div>
+		<p class="mx-auto text-2xl sm:text-3xl font-semibold text-center poppins">So why not invest!</p>
+	</section>
+</main>
