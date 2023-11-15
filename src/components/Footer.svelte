4206be1ac2dff56964ca2f9dd3ab6d33b916db72
Abhinav Dhiman <abhinav-ext@skyeair.tech>
2023-11-15 13:12:20 +0530

    Deployment gh-pages

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -15,10 +15,10 @@
         </div>
         <p class="mx-auto text-sm opacity-70">or</p>
         <p class="">Hugh_jass@gmail.com</p> -->
-		<p><b class="pr-2">Email</b> samuel_oak@gmail.com</p>
+		<p><b class="pr-2">Email</b> abhidhmn6@gmail.com</p>
 		<p>
 			<b class="pr-2">GitHub</b>
-			<a href=" " target="_blank" class="text-violet-400"
+			<a href="https://github.com/WanderFerb" target="_blank" class="text-violet-400"
 				>Abhinav Dhiman<sup class=""
 					><span class="text-xs scale-75 pl-0.5"
 						><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" /></span
@@ -28,7 +28,7 @@
 		</p>
 		<p>
 			<b class="pr-2">LinkedIn</b>
-			<a href=" " target="_blank" class="text-violet-400"
+			<a href="https://www.linkedin.com/in/abhindhiman/" target="_blank" class="text-violet-400"
 				>Abhinav Dhiman<sup class=""
 					><span class="text-xs scale-75 pl-0.5"
 						><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" /></span
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -40,7 +40,7 @@
 			</p>
 			<!-- svelte-ignore a11y-invalid-attribute -->
 			<button
-				href="#"
+				href="https://www.instagram.com/wanderferb/"
 				target="_blank"
 				class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
 			>
